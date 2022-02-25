782191c659c2fda03cad8c98034f3739f07a2620
Anonymous <80525761+phenias@users.noreply.github.com>
2022-02-25 23:21:09 +0200

    Update 2k.js

--- plugins/2k.js
+++ plugins/2k.js
@@ -3,9 +3,9 @@ let handler = async (m, { conn }) => {
   conn.sendFile(m.chat, 'https://www.reddit.com/r/RealGirls/random.json', '', 'sange~an', m)
   
 }
-handler.help = ['ass']
+handler.help = ['2k']
 handler.tags = ['anime']
-handler.command = /^(ass)$/i
+handler.command = /^(2k)$/i
 
 handler.limit = true
 handler.group = true
