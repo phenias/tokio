c4e45969889388cc4ef0e0c50ebce590b19e0eb2
Anonymous <80525761+phenias@users.noreply.github.com>
2022-02-25 23:56:35 +0200

    Update 2k.js

--- plugins/2k.js
+++ plugins/2k.js
@@ -4,18 +4,18 @@ let limit = 50
 let handler  = async (m, { conn, args, usedPrefix, command }) => {
 	await conn.updatePresence(m.chat, Presence.composing) 
 	let text = args.join` `
-	  	fetch('https://api.waifu.pics/nsfw/blowjob')
+	  	fetch('https://tikporntok.com/')
     	.then(res => res.json())
     	.then(json => {
     		conn.updatePresence(m.chat, Presence.composing) 
     		conn.reply(m.chat, `*Wait a moment . . .*`, m)
-	conn.sendFile(m.chat, json.url, 'blowjoc.mp4', '', m, false, { asDocument: true } )	
+	conn.sendFile(m.chat, json.url, 'tikporntok.mp4', '', m, false, { asGallery: true } )	
 	}) .catch(() => { conn.reply(m.chat, `*There is an error . . .*`, m) })
   			
 	}
-handler.help = ['blowjob']
+handler.help = ['tikporn']
 handler.tags = ['anime']
-handler.command = /^(blowjob)$/i
+handler.command = /^(tikporntok)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
